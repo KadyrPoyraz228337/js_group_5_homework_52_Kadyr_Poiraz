8eb670eef499986571390828b44fd784eb0dcd1b
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-10 18:36:52 +0200

    Добавили методы createRandomNumber и createBarrelValues в App

--- src/App.js
+++ src/App.js
@@ -15,6 +15,24 @@ class App extends Component {
         </div>
     );
   }
+    createRandomNumber(...array) {
+        const number = Math.floor(Math.random()*(36-5)-1)+5;
+
+        return array[0] === number || array[1] === number || array[2] === number || array[3] === number || array[4] === number ? this.createRandomNumber(...array) : number;
+    }
+
+    createBarrelValues() {
+        this.setState({
+            isClicked: false,
+            arrayValues: [],
+        });
+        for(let i = 0; i < 5; i++) {
+            this.state.arrayValues.push(this.createRandomNumber(this.state.arrayValues));
+        }
+        this.state.arrayValues.sort((a,b)=>{
+            return a - b;
+        });
+    }
 
 }
 
