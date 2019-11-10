bb39880164fea26cedad906c8281600108b3e3e2
Kadyr Poyraz <poyraz_2001@mail.ru>
2019-11-10 18:30:52 +0200

    Добавили новые компоненты Barrels и Barrel

---
+++ src/Components/barrels/barrel/Barrel.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Barrel extends Component{
+    render() {
+        return (
+            <div className="barrelElement">
+            </div>
+        )
+    }
+}
+
+export default Barrel;
